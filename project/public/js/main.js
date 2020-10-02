4e9b698433eb05dce915af926cd0db796d5eb440
Michel <lo.chelmi@gmail.com>
2020-10-02 17:05:33 +0200

    suite des pages jusqu a personnalisation

--- project/public/js/main.js
+++ project/public/js/main.js
@@ -1,20 +1,32 @@
 // upload fichier image
-let openFile = function(event) {
+let openFile = function (event) {
 
-  let input = event.target;
-  console.log(input)
+    let input = event.target;
+    console.log(input)
 
-  let reader = new FileReader();
-  if(input.type != "application/pdf"){
+    let reader = new FileReader();
+    if (input.type != "application/pdf") {
 
-  }
+    }
 
-  reader.onload = function(){
-    let dataURL = reader.result;
-    console.log(reader.result)
-    document.getElementById('data-file').value = reader.result;
-    let output = document.getElementById('output');
-    output.src = dataURL;
-  };
-  reader.readAsDataURL(input.files[0]);
+    reader.onload = function () {
+        let dataURL = reader.result;
+        console.log(reader.result)
+        document.getElementById('data-file').value = reader.result;
+        let output = document.getElementById('output');
+        output.src = dataURL;
+    };
+    reader.readAsDataURL(input.files[0]);
 };
+
+//controle de la quantité dans la pasge de produit
+
+let quantityProduct = document.querySelectorAll('.quantity-product');
+
+quantityProduct.forEach((quantity) => {
+    quantity.addEventListener('input', (e) => {
+        if (!Number(e.target.value)) {
+            e.target.value = ""
+        }
+    })
+})
--- project/src/Controller/PersonnalisationController.php
+++ project/src/Controller/PersonnalisationController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
 
@@ -13,7 +15,7 @@ class PersonnalisationController extends AbstractController
     private $category;
 
 
-    public function __construct( Environment $twig,CategoryRepository $categoryRepository)
+    public function __construct(Environment $twig, CategoryRepository $categoryRepository)
     {
         $this->twig = $twig;
         $this->category = $categoryRepository->findAll();
@@ -22,12 +24,46 @@ class PersonnalisationController extends AbstractController
 
     public function personnalisationClient($category, $product)
     {
+        $product = $this->getDoctrine()->getRepository(Product::class)->findOneBy(['name' => $product]);
+        if ($product) {
+            return $this->render('personnalisation/personnalisationClient.html.twig', [
+                'controller_name' => 'PersonnalisationController',
+                'categories' => $this->category,
+                'theCategory' => $category,
+                'theProduct' => $product
+            ]);
+        }else{
+            return $this->render('bundles/TwigBundle/Execption/error404.html.twig', [
+                'categories' => $this->category,
+            ]);
+        }
+    }
 
-        return $this->render('personnalisation/personnalisationClient.html.twig', [
-            'controller_name' => 'PersonnalisationController',
-            'categories'=> $this->category,
-            'theCategory' => $category,
-            'theProduct' => $product
-        ]);
+    public function personnalisationCheckInfo(Request $request)
+    {
+
+        $i = 0;
+        foreach ($request->request->all() as $value) {
+            if (!empty($value && $value > 49)) {
+                $i++;
+            }
+        }
+        dump($i);
+        if ($i <= 0) {
+            $this->addFlash('errorQuantity', "il n'y pas de quantity");
+            return $this->redirectToRoute('productClient', [
+                'category' => $request->attributes->get('category'),
+                'product' => $request->attributes->get('product'),
+                'id' => $request->attributes->get('id'),
+                'color' => $request->attributes->get('color'),
+            ]);
+        } else {
+            return $this->redirectToRoute('personnalisationClient', [
+                'category' => $request->attributes->get('category'),
+                'product' => $request->attributes->get('product'),
+                'id' => $request->attributes->get('id'),
+                'color' => $request->attributes->get('color'),
+            ]);
+        }
     }
 }
