b5142f89d71def3d144cf6b5d5df7c9c4611ab2d
Michel <lo.chelmi@gmail.com>
2020-10-05 16:38:08 +0200

    personnalisation designe et commencement des images

--- project/public/js/main.js
+++ project/public/js/main.js
@@ -19,14 +19,22 @@ let openFile = function (event) {
     reader.readAsDataURL(input.files[0]);
 };
 
-//controle de la quantité dans la pasge de produit
+//controle de la quantité dans la pasge de produit et bution est déactiver qsi il n'a pas de value
 
 let quantityProduct = document.querySelectorAll('.quantity-product');
+let btn = document.querySelector('.displayBtn')
 
 quantityProduct.forEach((quantity) => {
     quantity.addEventListener('input', (e) => {
         if (!Number(e.target.value)) {
             e.target.value = ""
+        }else{
+            if(e.target.value >= 50){
+                btn.disabled = false;
+            }else{
+                btn.disabled = true;
+            }
         }
     })
 })
+
