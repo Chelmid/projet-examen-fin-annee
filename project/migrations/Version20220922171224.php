746ebd75d02bc4c323b60403ee2239421431a341
Michel <lo.chelmi@gmail.com>
2022-09-22 19:28:02 +0200

    update

---
+++ project/migrations/Version20220922171224.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220922171224 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE `order` (id INT AUTO_INCREMENT NOT NULL, panier_id INT NOT NULL, user_id INT NOT NULL, total_price DOUBLE PRECISION NOT NULL, date_order DATETIME NOT NULL, method_payement VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_F5299398F77D927C (panier_id), INDEX IDX_F5299398A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_general_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE panier (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, is_order TINYINT(1) NOT NULL, INDEX IDX_24CC0DF2A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_general_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE panier_product (id INT AUTO_INCREMENT NOT NULL, personnalisation_id INT DEFAULT NULL, product_id INT NOT NULL, panier_id INT NOT NULL, quantity INT NOT NULL, color_and_image VARCHAR(255) NOT NULL, INDEX IDX_29F0C02C3DD790BB (personnalisation_id), INDEX IDX_29F0C02C4584665A (product_id), INDEX IDX_29F0C02CF77D927C (panier_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_general_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE personnalisation (id INT AUTO_INCREMENT NOT NULL, price_impression_id INT NOT NULL, file VARCHAR(255) NOT NULL, top_position VARCHAR(255) NOT NULL, left_position VARCHAR(255) NOT NULL, width INT NOT NULL, height INT NOT NULL, datauri LONGTEXT NOT NULL, impression VARCHAR(255) NOT NULL, INDEX IDX_C9B3EAE6C048F903 (price_impression_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_general_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE price_impression (id INT AUTO_INCREMENT NOT NULL, type VARCHAR(255) NOT NULL, price DOUBLE PRECISION NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_general_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE product (id INT AUTO_INCREMENT NOT NULL, category_id INT NOT NULL, name VARCHAR(255) NOT NULL, sku VARCHAR(255) NOT NULL, color VARCHAR(255) NOT NULL, price DOUBLE PRECISION NOT NULL, image VARCHAR(255) NOT NULL, create_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, description VARCHAR(255) DEFAULT NULL, quantity VARCHAR(255) NOT NULL, INDEX IDX_D34A04AD12469DE2 (category_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_general_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE zone_de_marquage (id INT AUTO_INCREMENT NOT NULL, product_id INT NOT NULL, height INT DEFAULT NULL, width INT DEFAULT NULL, left_space INT DEFAULT NULL, top_space INT DEFAULT NULL, UNIQUE INDEX UNIQ_5762182E4584665A (product_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_general_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE `order` ADD CONSTRAINT FK_F5299398F77D927C FOREIGN KEY (panier_id) REFERENCES panier (id)');
+        $this->addSql('ALTER TABLE `order` ADD CONSTRAINT FK_F5299398A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE panier ADD CONSTRAINT FK_24CC0DF2A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE panier_product ADD CONSTRAINT FK_29F0C02C3DD790BB FOREIGN KEY (personnalisation_id) REFERENCES personnalisation (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE panier_product ADD CONSTRAINT FK_29F0C02C4584665A FOREIGN KEY (product_id) REFERENCES product (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE panier_product ADD CONSTRAINT FK_29F0C02CF77D927C FOREIGN KEY (panier_id) REFERENCES panier (id)');
+        $this->addSql('ALTER TABLE personnalisation ADD CONSTRAINT FK_C9B3EAE6C048F903 FOREIGN KEY (price_impression_id) REFERENCES price_impression (id)');
+        $this->addSql('ALTER TABLE product ADD CONSTRAINT FK_D34A04AD12469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
+        $this->addSql('ALTER TABLE zone_de_marquage ADD CONSTRAINT FK_5762182E4584665A FOREIGN KEY (product_id) REFERENCES product (id)');
+        $this->addSql('ALTER TABLE user ADD groupclient INT NOT NULL, ADD adresse VARCHAR(255) NOT NULL, ADD cp INT NOT NULL, ADD ville VARCHAR(255) NOT NULL, ADD telephone INT NOT NULL, ADD pays VARCHAR(255) NOT NULL, CHANGE number_tva number_tva VARCHAR(20) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE `order` DROP FOREIGN KEY FK_F5299398F77D927C');
+        $this->addSql('ALTER TABLE panier_product DROP FOREIGN KEY FK_29F0C02CF77D927C');
+        $this->addSql('ALTER TABLE panier_product DROP FOREIGN KEY FK_29F0C02C3DD790BB');
+        $this->addSql('ALTER TABLE personnalisation DROP FOREIGN KEY FK_C9B3EAE6C048F903');
+        $this->addSql('ALTER TABLE panier_product DROP FOREIGN KEY FK_29F0C02C4584665A');
+        $this->addSql('ALTER TABLE zone_de_marquage DROP FOREIGN KEY FK_5762182E4584665A');
+        $this->addSql('DROP TABLE `order`');
+        $this->addSql('DROP TABLE panier');
+        $this->addSql('DROP TABLE panier_product');
+        $this->addSql('DROP TABLE personnalisation');
+        $this->addSql('DROP TABLE price_impression');
+        $this->addSql('DROP TABLE product');
+        $this->addSql('DROP TABLE zone_de_marquage');
+        $this->addSql('ALTER TABLE user DROP groupclient, DROP adresse, DROP cp, DROP ville, DROP telephone, DROP pays, CHANGE number_tva number_tva VARCHAR(20) CHARACTER SET utf8 NOT NULL COLLATE `utf8_unicode_ci`');
+    }
+}
